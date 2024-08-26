012de761a0fea13e3d69e150db5319143c3e51e3
Arsul jawed <Arsuljawed97@gmail.com.com>
2024-08-26 22:18:39 +0530

    Update .gitignore and add .angular directory

---
+++ .angular/cache/18.2.0/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,24 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-4QCXOTH7.js";
+import "./chunk-B5ORDGJM.js";
+import "./chunk-RSL6BPJO.js";
+import "./chunk-HCFBPZ3H.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-VAMGWXBZ.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
