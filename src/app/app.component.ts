6784d2cd7027bf2c204a9b264e76e9aa97941880
Arsul jawed <Arsuljawed97@gmail.com.com>
2024-08-27 14:10:35 +0530

    bug fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -12,6 +14,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'budget-planner';
+// export class AppComponent {
+//   title = 'budget-planner';
+// }
+
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Check if the current URL is the root URL
+    if (this.router.url === '/') {
+      this.router.navigate(['/budget-planner/login']);
+    }
+  }
 }
+
