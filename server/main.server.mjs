e475aef9d3ea2e22447f959e226629295c2fc2b1
Arsul jawed <Arsuljawed97@gmail.com.com>
2024-08-27 17:10:40 +0530

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-QSETYPO7.mjs";import"./chunk-LDP7SEYO.mjs";import"./chunk-Z5F7HU5G.mjs";import"./chunk-5YQI3JQJ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
